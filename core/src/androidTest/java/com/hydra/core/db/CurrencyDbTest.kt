71aaf2fb4d443bcc0dc37761b2df1803e407ba9f
hydra <hydrawork@gmail.com>
2021-01-16 12:57:26 +0800

    Write room unit test to quarantee dao works

---
+++ core/src/androidTest/java/com/hydra/core/db/CurrencyDbTest.kt
@@ -0,0 +1,45 @@
+package com.hydra.core.db
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class CurrencyDbTest {
+    private lateinit var dao: CurrencyDao
+    private lateinit var db: CurrencyDb
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(
+            context, CurrencyDb::class.java
+        ).build()
+        dao = db.currencyDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun writeAndQueryCurrency() {
+        val currency = Currency("title", "value")
+        dao.insert(currency)
+        runBlocking {
+            val value = dao.queryByTitle("title")
+            assert(value.description == "value")
+        }
+    }
+}
--- core/src/main/java/com/hydra/core/db/CurrencyDao.kt
+++ core/src/main/java/com/hydra/core/db/CurrencyDao.kt
@@ -3,9 +3,16 @@ package com.hydra.core.db
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface CurrencyDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(currencies: List<Currency>)
+
+    @Insert
+    fun insert(currency: Currency)
+
+    @Query("SELECT * FROM currency WHERE title = :title")
+    suspend fun queryByTitle(title: String): Currency
 }
