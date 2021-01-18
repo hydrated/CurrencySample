c3ea6d0e6aac60ab5e47a79097b0f2f7a946f9a0
hydra <hydrawork@gmail.com>
2021-01-18 12:13:49 +0800

    Basic UI structure for the app

--- app/src/main/java/com/hydra/currencysample/activity/MainActivity.kt
+++ app/src/main/java/com/hydra/currencysample/activity/MainActivity.kt
@@ -1,36 +1,44 @@
-package com.hydra.currencysample
+package com.hydra.currencysample.activity
 
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import com.hydra.core.network.HttpClient
+import com.hydra.currencysample.R
+import com.hydra.currencysample.adapter.ExchangeRateAdapter
 import com.hydra.currencysample.viewmodel.MainViewModel
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
-import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: MainViewModel by inject()
+    private lateinit var adapter: ExchangeRateAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setupUi()
         setupObservers()
-        viewModel.getAvailableCurrency()
+        setupData()
+    }
 
+    private fun setupUi()  {
+        adapter = ExchangeRateAdapter()
+        recyclerView.adapter = adapter
+    }
+
+    private fun setupData() {
+        viewModel.getAvailableCurrency()
+        viewModel.getExchangeRate()
     }
 
     private fun setupObservers() {
         viewModel.currencies.observe(this, Observer {
-            Log.d("hydrated", "" + it)
+        })
+        viewModel.rateList.observe(this, Observer {
+            adapter.submitList(it)
         })
     }
 
---
+++ app/src/main/java/com/hydra/currencysample/adapter/ExchangeRateAdapter.kt
@@ -0,0 +1,59 @@
+package com.hydra.currencysample.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.hydra.core.model.ExchangeRate
+import com.hydra.currencysample.R
+import kotlinx.android.synthetic.main.item_exchange_rate.view.*
+import java.text.NumberFormat
+
+class ExchangeRateAdapter :
+    ListAdapter<ExchangeRate, ExchangeRateAdapter.RateHolder>(ExchangeRateDiff()) {
+    private var amount: Double = 0.0
+    private var ratioToUsb: Double = 1.0
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RateHolder {
+        return RateHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_exchange_rate,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RateHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    fun updateAmountAndRatio(amount: Double, ratio: Double) {
+        this.amount = amount
+        this.ratioToUsb = ratio
+        notifyDataSetChanged()
+    }
+
+    inner class RateHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(rate: ExchangeRate) {
+            with(view) {
+                title.text = rate.country
+                exchange_rate.text =
+                    NumberFormat.getNumberInstance().format(rate.rate)
+                value.text =
+                    NumberFormat.getNumberInstance().format(rate.rate * amount * ratioToUsb)
+            }
+        }
+    }
+}
+
+
+class ExchangeRateDiff : DiffUtil.ItemCallback<ExchangeRate>() {
+    override fun areItemsTheSame(oldItem: ExchangeRate, newItem: ExchangeRate) =
+        oldItem.country == newItem.country
+
+    override fun areContentsTheSame(oldItem: ExchangeRate, newItem: ExchangeRate) =
+        oldItem == newItem
+}
